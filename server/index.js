cc2d8b9e8cc3a5a621db358a0c814714d1f71965
k2poni00 <112403500+k2poni00@users.noreply.github.com>
2022-11-21 18:35:57 +0200

    Installed and set up mysql2.

--- server/index.js
+++ server/index.js
@@ -1,15 +1,25 @@
 const express = require("express");
 const cors = require("cors");
+const mysql = require("mysql2/promise");
+const config = require("./config");
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
-app.use(cors());
 
-app.get("/", (req, res) => {
-    res.status(200).json({message: "Node server is responding"})
-})
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
 
 app.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`);
+});
+
+app.get("/", async function (req, res) {
+    try {
+        const connection = await mysql.createConnection(config.db);
+        res.status(200).json({message: "Database connection was made"})
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
 });
