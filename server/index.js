adf0af55db56c6cad399b0d435fbd0d1cd23db15
k2poni00 <112403500+k2poni00@users.noreply.github.com>
2022-11-21 13:02:56 +0200

    Created Node and React project with express and nodemon.

---
+++ server/index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.get("/", (req, res) => {
+    res.status(200).json({message: "Node server is responding"})
+})
+
+app.listen(PORT, () => {
+    console.log(`Server listening on ${PORT}`);
+});
