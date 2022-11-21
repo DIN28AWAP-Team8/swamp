f64e441eb2338e8dfb4ebf8db5a2bc496e0b71fd
k2poni00 <112403500+k2poni00@users.noreply.github.com>
2022-11-21 13:38:02 +0200

    Set up cors.

--- server/index.js
+++ server/index.js
@@ -1,6 +1,10 @@
 const express = require("express");
+const cors = rquires("cors");
+
 const PORT = process.env.PORT || 3001;
+
 const app = express();
+app.use(cors());
 
 app.get("/", (req, res) => {
     res.status(200).json({message: "Node server is responding"})
