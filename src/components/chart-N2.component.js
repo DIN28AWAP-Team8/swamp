05de89eb0b62f2830d50f0bc0f034bbf62a65530
k2poni00 <112403500+k2poni00@users.noreply.github.com>
2022-12-06 11:36:49 +0200

    Added V9 to N2

--- src/components/chart-N2.component.js
+++ src/components/chart-N2.component.js
@@ -1,11 +1,15 @@
 import React from "react";
 import V8Chart from "./charts/V8";
+import V9Chart from "./charts/V9";
 
 function N2() {
   return (
     <div className="container">
       <div>
         <V8Chart />
+      </div>
+      <div>
+        <V9Chart />
       </div>
     </div>
   );
